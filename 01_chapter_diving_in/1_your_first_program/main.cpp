25f225ee4f5b5250eb8f5e670a55169968672f5f
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-10-14 00:38:24 +0500

    added first C++ code to tutorial directory

---
+++ 01_chapter_diving_in/1_your_first_program/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+void logMessage(std::vector<std::string> messageList) {
+  for (int i = 0; i < messageList.size(); ++i) {
+    std::cout << messageList[i] << std::endl;
+  }
+}
+
+int main() {
+  logMessage({"C++ log message", "Another C++ log message", "Last C++ log message!"});
+  /* Logs:
+    C++ log message
+    Another C++ log message
+    Last C++ log message!
+  */
+
+  return 0;
+}
