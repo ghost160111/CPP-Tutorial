ac17b82c16381a9c9173e3464e72e813272a6ab6
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-10-15 13:00:50 +0500

    added static class example

--- 01_chapter_diving_in/1_your_first_program/main.cpp
+++ 01_chapter_diving_in/1_your_first_program/main.cpp
@@ -2,14 +2,33 @@
 #include <string>
 #include <vector>
 
+// static Library 
+static class Library {
+public:
+  static void log(std::vector<std::string> logMessages) {
+    for (int i = 0; i < logMessages.size(); ++i) {
+      std::cout << logMessages[i] << std::endl;
+    }
+  }
+};
+
+// global function
 void logMessage(std::vector<std::string> messageList) {
   for (int i = 0; i < messageList.size(); ++i) {
     std::cout << messageList[i] << std::endl;
   }
 }
 
+// entry point of program
 int main() {
   logMessage({"C++ log message", "Another C++ log message", "Last C++ log message!"});
+  /* Logs:
+    C++ log message
+    Another C++ log message
+    Last C++ log message!
+  */
+  
+  Library::log({"C++ log message", "Another C++ log message", "Last C++ log message!"});
   /* Logs:
     C++ log message
     Another C++ log message
